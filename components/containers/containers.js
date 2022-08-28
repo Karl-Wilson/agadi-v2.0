dd871860f1cfeb6ec34de088f1b91ac713e7120f
karl-wilson <okosechidelu@gmail.com>
2022-08-28 15:32:51 +0100

    add contentContainer

--- components/containers/containers.js
+++ components/containers/containers.js
@@ -11,6 +11,7 @@ import DrugEntry from "./drugEntry";
 import SearchBar from "./searchBar";
 import IndicatorContainer from "./indicatorContainer";
 import CardHolder from "./cardHolders";
+import ContentContainer from "./contentContainer";
 export {FormButtonContainer, FormGroup, FormInputContainer, FormTitleContainer, HeroareaContent, HeroareaImage, 
         InputGroup, PageContainer, HeightInputContainer, WeightInputContainer, BPInputContainer, DrugEntry, 
-        SearchBar, IndicatorContainer, CardHolder}
+        SearchBar, IndicatorContainer, CardHolder, ContentContainer}
