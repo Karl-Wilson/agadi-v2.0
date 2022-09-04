fcca588f290168ba725b4279479690e73834cdf0
karl-wilson <okosechidelu@gmail.com>
2022-09-04 08:20:00 +0100

    update height css

--- components/layouts/displayWindow.js
+++ components/layouts/displayWindow.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 import {ContentContainer} from '../containers/containers'
 const Wrapper = styled.div`
     width: 100%;
-    height: 100%;
+    height: auto;
     position: relative;
     top: 88px;
     z-index: 1;
