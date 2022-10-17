6822ca7aae81baaa02bcef87976a9c4386de667c
karl-wilson <okosechidelu@gmail.com>
2022-10-17 22:39:23 +0100

    update dropdown shadow and color

--- components/layouts/nav.js
+++ components/layouts/nav.js
@@ -23,9 +23,11 @@ const ButtonWrapper = styled.div`
             flex-direction: column;
             align-items: center;
             width: 100%; 
-            background-color: #eeeeee;
+            background-color: #ffffff;
             padding: 20px 0px;
             z-index: 14;
+            border-radius: 10px;
+            box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);
         }
     
     @media screen and (min-width: 768px) {
